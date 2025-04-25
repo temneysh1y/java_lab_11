49b30d9ce2ddfb772e477f8dc3f940dfff82bb9e
temneysh1y <Lebedevartem2021@gmail.com>
2025-04-25 11:34:07 +0300

    Первый коммит: проект AnimalHierarchy

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public abstract class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+    public abstract void move();
+}
